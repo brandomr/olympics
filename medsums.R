0d1e874959b4566ae0604db1be25075a23375cbd
brandomr <rose.brandon.m@gmail.com>
2014-02-17 20:12:19 -0500

    Get total medals by country

--- medsums.R
+++ medsums.R
@@ -1,5 +1,28 @@
-totalcount <- with(medcount, tapply(Total, list(partCountry, GameType), sum))
+gettotals <- function(data, sortby="Country") {
+  
+  totalcount <- with(data, tapply(TotalAdj, list(Country.Name, GameType), sum))
 
-dim(totalcount)
+  dim(totalcount)
 
-lapply(1:2, function(i) totalcount[,i])
+  totalcount <- data.frame(lapply(1:2, function(i) totalcount[,i]))
+  colnames(totalcount) <- c("summer.medcount", "winter.medcount")
+
+  totalcount["Country.Name"] <- rownames(totalcount)
+  totalcount <- totalcount[c(3,1,2)]
+  row.names(totalcount) <- NULL
+  
+  if(sortby == "Summer") {  
+    totalcount <- totalcount[with(totalcount, order(-totalcount[,2])),]
+  }
+
+  if(sortby == "Winter") {  
+    totalcount <- totalcount[with(totalcount, order(-totalcount[,3])),]
+  }
+  
+  if(sortby == "Country") {  
+    
+  }
+    
+  print(head(totalcount))
+  
+}
