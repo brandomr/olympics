eaf3fe063c613aabe629a955d592ddd795b2352c
brandomr <rose.brandon.m@gmail.com>
2014-03-03 16:56:31 -0500

    lattice plot code
    updated lattice plot code for R

--- create_lattice.R
+++ create_lattice.R
@@ -2,18 +2,19 @@ createlattice <- function(data, sortby="Summer") {
   
   gametypesub <- data[ which(data$GameType== sortby), ]
   
- 
+  
   #assign('gametypesub', gametypesub, envir=.GlobalEnv)
   
   library(lattice)
   
   gametypesub$Year <- as.factor(gametypesub$Year)
   
-  xyplot(TotalAdj ~ Pop | Year, 
+  xyplot(TotalAdj ~ log(GDP) | Year, 
          data = gametypesub,
-         xlab = "Population",
+         xlab = "Log(GDP)",
          ylab = "Total Medals Earned",
-         main = "Winter Olympics: Medal Count by Population",
+         main = "Winter Olympics: Medal Count by Log(GDP)",
+         layout = c(4,4),
          panel = function(x,y,...,subscripts){
            panel.xyplot(x,y)
            panel.lmline(x,y, col="grey")
